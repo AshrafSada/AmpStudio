bd2a6ea4aee63f395a62ca0bbbdf9e4c3962ec23
Ashraf Sada <ashraf.sada@outlook.com>
2023-04-27 20:17:49 +0300

    - Check-in changes

---
+++ AmpCore/AmpCore.cpp
@@ -0,0 +1,9 @@
+// AmpCore.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+int Testing() {
+    return 200;
+}
---
+++ StudioMain/Main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <AmpCore/AmpCore.h>
+
+int main() {
+    printf("%i", Testing());
+
+    return 0;
+}
